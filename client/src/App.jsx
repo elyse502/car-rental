7cb43053efab77e27ca0c812480309f8fcadb582
elyse502 <elyseniyibizi502@gmail.com>
2025-08-01 09:10:49 +0200

    feat: Hide Navbar on owner dashboard routes
    - Used useLocation to detect current path
    
    - Conditionally rendered Navbar only if pathname doesn't start with '/owner'

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from "react";
 import Navbar from "./components/Navbar";
+import { useLocation } from "react-router-dom";
 
 const App = () => {
   const [showLogin, setShowLogin] = useState(false);
+  const isOwnerPath = useLocation().pathname.startsWith("/owner");
 
   return (
     <>
-      <Navbar setShowLogin={setShowLogin} />
+      {!isOwnerPath && <Navbar setShowLogin={setShowLogin} />}
     </>
   );
 };
