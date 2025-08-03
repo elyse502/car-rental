b74216e7f3ac0c19cb75bce52776d6b2a634b68b
elyse502 <elyseniyibizi502@gmail.com>
2025-08-03 07:48:39 +0200

    feat(owner): Implement SideBar with user profile and navigation
      - Added editable user profile image to SideBar
    
      - Displayed dynamic user info from dummy data
    
      - Rendered navigation links with active styles
    
      - Integrated SideBar and Outlet into Layout for nested routing

--- client/src/pages/owner/Layout.jsx
+++ client/src/pages/owner/Layout.jsx
@@ -1,9 +1,15 @@
+import { Outlet } from "react-router-dom";
 import NavbarOwner from "../../components/owner/NavbarOwner";
+import SideBar from "../../components/owner/SideBar";
 
 const Layout = () => {
   return (
-    <div>
+    <div className="flex flex-col">
       <NavbarOwner />
+      <div className="flex">
+        <SideBar />
+        <Outlet />
+      </div>
     </div>
   );
 };
