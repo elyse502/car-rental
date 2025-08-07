be690e8b70e91ac2de9510cfba2883c31da95a3a
elyse502 <elyseniyibizi502@gmail.com>
2025-08-07 19:37:29 +0200

    feat(controllers): Add checkAvailability function to bookingController
        - Adds logic to verify car availability within a date range
    
        - Queries bookings with overlapping pickup and return dates
    
        - Returns true if no conflicts found

---
+++ server/controllers/bookingController.js
@@ -0,0 +1,12 @@
+import Booking from "../models/Booking.js";
+
+// Function to Check Availability of Car for a given Date
+const checkAvailability = async (car, pickupDate, returnDate) => {
+  const bookings = await Booking.find({
+    car,
+    pickupDate: { $lte: returnDate },
+    returnDate: { $gte: pickupDate },
+  });
+
+  return bookings.length === 0;
+};
