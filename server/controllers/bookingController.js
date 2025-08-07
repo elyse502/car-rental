dd09e8b7b891cc15dcb0c705333790700cdea378
elyse502 <elyseniyibizi502@gmail.com>
2025-08-07 22:32:36 +0200

    feat(booking): Add APIs to fetch bookings for users and car owners
        - Implemented 'getUserBookings' controller to retrieve bookings made by the logged-in user
    
        - Implemented 'getOwnerBookings' controller for car owners to view bookings of their cars
    
        - Includes population of car and user details with sorting by creation date
    
        - Added role check to restrict access for non-owners

--- server/controllers/bookingController.js
+++ server/controllers/bookingController.js
@@ -77,3 +77,37 @@ export const createBooking = async (req, res) => {
     res.json({ success: false, message: error.message });
   }
 };
+
+// API to List User Bookings
+export const getUserBookings = async (req, res) => {
+  try {
+    const { _id } = req.user;
+    const bookings = await Booking.find({ user: _id })
+      .populate("car")
+      .sort({ createdAt: -1 });
+
+    res.json({ success: true, bookings });
+  } catch (error) {
+    console.log(error.message);
+    res.json({ success: false, message: error.message });
+  }
+};
+
+//API to get Owner Bookings
+export const getOwnerBookings = async (req, res) => {
+  try {
+    if (req.user.role !== "owner") {
+      return res.json({ success: false, message: "Unauthorized" });
+    }
+
+    const bookings = await Booking.find({ owner: req.user._id })
+      .populate("car user")
+      .select("-user.password")
+      .sort({ createdAt: -1 });
+
+    res.json({ success: true, bookings });
+  } catch (error) {
+    console.log(error.message);
+    res.json({ success: false, message: error.message });
+  }
+};
