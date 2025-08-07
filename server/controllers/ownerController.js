a11eeeccbccdc5c88e29ce42bf4fa228a79b4bd8
elyse502 <elyseniyibizi502@gmail.com>
2025-08-07 06:49:15 +0200

    feat: Add car listing functionality with image upload via ImageKit
        - Implemented 'addCar' API in 'ownerController.js' to allow owners to upload car listings
    
        - Integrated ImageKit for optimized image uploading and CDN delivery
    
        - Added multer middleware for handling image uploads
    
        - Created ImageKit config file for secure API integration
    
        - Updated dependencies: added 'imagekit' and 'multer' packages

--- server/controllers/ownerController.js
+++ server/controllers/ownerController.js
@@ -1,11 +1,50 @@
+import imagekit from "../configs/imageKit.js";
+import Car from "../models/Car.js";
 import User from "../models/User.js";
+import fs from "fs";
 
+// API to change Role of User
 export const changeRoleToOwner = async (req, res) => {
   try {
     const { _id } = req.user;
     await User.findByIdAndUpdate(_id, { role: "owner" });
 
     res.json({ success: true, message: "Now you can list cars" });
+  } catch (error) {
+    console.log(error.message);
+    res.json({ success: false, message: error.message });
+  }
+};
+
+// API to List Car
+export const addCar = async (req, res) => {
+  try {
+    const { _id } = req.user;
+    let car = JSON.parse(req.body.carData);
+    const imageFile = req.file;
+
+    // Upload Image to ImageKit
+    const fileBuffer = fs.readFileSync(imageFile.path);
+    const response = await imagekit.upload({
+      file: fileBuffer,
+      fileName: imageFile.originalname,
+      folder: "/cars",
+    });
+
+    // Optimization through imageKit URL transformation
+    var optimezedImageUrl = imagekit.url({
+      path: response.filePath,
+      transformation: [
+        { width: "1280" }, // Width resizing
+        { quality: "auto" }, // Auto compression
+        { format: "webp" }, // Convert to modern format
+      ],
+    });
+
+    const image = optimezedImageUrl;
+    await Car.create({ ...car, owner: _id, image });
+
+    res.json({ success: true, message: "Car Added" });
   } catch (error) {
     console.log(error.message);
     res.json({ success: false, message: error.message });
