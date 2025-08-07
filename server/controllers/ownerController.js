bdbabb10bf06f18fb77bcb902f3c9b3c40a7c81f
elyse502 <elyseniyibizi502@gmail.com>
2025-08-07 07:20:55 +0200

    feat(controller): Implement handler to fetch owner-specific cars
        - Added 'getOwnerCars' controller to retrieve cars owned by the logged-in user
    
        - Ensures only authenticated owners can view their car listings

--- server/controllers/ownerController.js
+++ server/controllers/ownerController.js
@@ -50,3 +50,16 @@ export const addCar = async (req, res) => {
     res.json({ success: false, message: error.message });
   }
 };
+
+// API to List Owner Cars
+export const getOwnerCars = async (req, res) => {
+  try {
+    const { _id } = req.user;
+    const cars = await Car.find({ owner: _id });
+
+    res.json({ success: true, cars });
+  } catch (error) {
+    console.log(error.message);
+    res.json({ success: false, message: error.message });
+  }
+};
