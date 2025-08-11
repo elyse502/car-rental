2c3df511a6a40f18038200dbe8526081986772bf
elyse502 <elyseniyibizi502@gmail.com>
2025-08-11 07:31:54 +0200

    feat(owner-layout): Restrict access to owners and redirect non-owners
        - Imported useAppContext and useEffect in Layout.jsx
    
        - Added check for isOwner from AppContext
    
        - Redirected users to home page ('/') if not owners

--- client/src/pages/owner/Layout.jsx
+++ client/src/pages/owner/Layout.jsx
@@ -1,8 +1,17 @@
 import { Outlet } from "react-router-dom";
 import NavbarOwner from "../../components/owner/NavbarOwner";
 import SideBar from "../../components/owner/SideBar";
+import { useAppContext } from "../../context/AppContext";
+import { useEffect } from "react";
 
 const Layout = () => {
+  const { isOwner, navigate } = useAppContext();
+
+  useEffect(() => {
+    if (!isOwner) {
+      navigate("/");
+    }
+  }, [isOwner]);
   return (
     <div className="flex flex-col">
       <NavbarOwner />
